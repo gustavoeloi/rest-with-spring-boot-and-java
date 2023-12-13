b4f378d6630a7bf328b6c2a2e84f3b5a6475b5a4
gustavooeloi <gustavooeloi@gmail.com>
2023-12-13 10:37:17 -0300

    First Steps With Java and Spring

---
+++ 05_FirstStepsInJavaWithSpringBoot/first-steps-with-java-spring/src/main/java/br/com/erudio/FirstStepsWithJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsWithJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStepsWithJavaSpringApplication.class, args);
+	}
+
+}
