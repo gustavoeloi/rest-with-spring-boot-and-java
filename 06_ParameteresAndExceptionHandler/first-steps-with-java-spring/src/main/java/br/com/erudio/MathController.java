ce82b8bd6a7c9acffc20292eaac7806cf410bdf0
Gustavo Eloi <gustavooeloi@gmail.com>
2023-12-14 10:07:40 -0300

    adding mathematical operations REST

--- 06_ParameteresAndExceptionHandler/first-steps-with-java-spring/src/main/java/br/com/erudio/MathController.java
+++ 06_ParameteresAndExceptionHandler/first-steps-with-java-spring/src/main/java/br/com/erudio/MathController.java
@@ -1,5 +1,6 @@
 package br.com.erudio;
 
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +13,51 @@ public class MathController {
     public Double sum(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedOperationException("Please set a numeric value!");
         }
         return covertToDouble(numberOne) + covertToDouble(numberTwo);
-    }    
-    
+    }
+
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return covertToDouble(numberOne) - covertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/multi/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multi(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return covertToDouble(numberOne) * covertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return covertToDouble(numberOne) / covertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double average(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping(value = "/square/{numberOne}", method = RequestMethod.GET)
+    public Double average(@PathVariable("numberOne") String numberOne){
+        if(!isNumeric(numberOne)){
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return Math.sqrt(covertToDouble(numberOne));
+    }
+
     public static Double covertToDouble(String strNumber) {
         if (strNumber == null) return 0d; 
         String number = strNumber.replaceAll(",", ".");
